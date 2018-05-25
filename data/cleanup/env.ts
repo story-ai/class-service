894e899e47837ddee4176b044ba03df2316d18d5
Christopher Little <hephistocles@gmail.com>
2018-05-25 11:29:39 +0100

    type loaded yaml

--- data/cleanup/env.ts
+++ data/cleanup/env.ts
@@ -1,10 +1,19 @@
 import * as yaml from "js-yaml"; //yaml = require('js-yaml');
 import * as fs from "fs";
+import { MAILCHIMP_API_KEY } from "../../config";
 
 export function setEnv() {
   // Get document, or throw exception on error
   try {
-    var doc = yaml.safeLoad(fs.readFileSync("keys.yml", "utf8"));
+    var doc = yaml.safeLoad(fs.readFileSync("keys.yml", "utf8")) as {
+      CENTURY_ADMIN_EMAIL: string;
+      CENTURY_ADMIN_PASSWORD: string;
+      STRIPE_SECRET_KEY: {
+        dev: string;
+        prod: string;
+      };
+      MAILCHIMP_API_KEY: string;
+    };
     const env = {
       CENTURY_ADMIN_EMAIL: doc.CENTURY_ADMIN_EMAIL,
       CENTURY_ADMIN_PASSWORD: doc.CENTURY_ADMIN_PASSWORD,
